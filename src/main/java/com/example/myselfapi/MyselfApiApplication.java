d9fcfcb01ce73b1b6fa3d17cd263cba9dd598958
Kiss <mrkiss.it@gmail.com>
2025-05-29 23:33:29 +0700

    Initial commit

---
+++ src/main/java/com/example/myselfapi/MyselfApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.myselfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselfApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyselfApiApplication.class, args);
+    }
+}
